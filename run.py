7ca6f91ddb7c1098238b1ad7ae1adc2785f991f5
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-05-18 01:07:46 +0200

    refactored code a bit to make it more readable.

--- run.py
+++ run.py
@@ -8,11 +8,19 @@ import csv
 import config
 
 
-# Build Stimuli and Position them.s
+# Make Psychopy visual stimuli
+window = visual.Window(fullscr=config.FULLSCREEN, allowStencil=True, color=config.BGCOLOR)
+fixcross = visual.TextStim(window, text='+', alignVert='center', alignHoriz='center')
+
+# Make Ratcave stimuli
 reader = rc.WavefrontReader('stimuli/rotstim.obj')
 stim_orig = reader.get_mesh('OrigStim', position=(-.25, 0., -1), rotation=(0, config.YROT, 0), scale=config.SCALE)
 stim_orig2 = reader.get_mesh('OrigStim', position = (.25, 0., -1), rotation=(0, config.YROT, 0), scale=config.SCALE)
 stim_flipped = reader.get_mesh('FlippedStim', position = (.25, 0., -1), rotation=(0, config.YROT, 0), scale=config.SCALE)
+
+scene = rc.Scene(meshes=[])
+scene.light.position.z = 20
+scene.camera.projection = rc.OrthoProjection(origin='center', coords='relative')
 
 # Make Groups of Stimuli to refer to in experiment
 stims = [stim_orig, stim_orig2, stim_flipped]
@@ -20,7 +28,6 @@ condA = [stim_orig, stim_orig2]
 condB = [stim_orig, stim_flipped]
 
 
-window = visual.Window(fullscr=config.FULLSCREEN, allowStencil=True)
 
 def show_instructions():
     msg = """
@@ -36,14 +43,6 @@ def show_instructions():
     event.waitKeys()
 show_instructions()
 
-scene = rc.Scene(meshes=[], bgColor=config.BGCOLOR)
-scene.light.position.z = 20
-scene.camera.projection = rc.OrthoProjection(origin='center', coords='relative')
-camera = scene.camera
-
-
-fixcross = visual.TextStim(window, text='+', alignVert='center', alignHoriz='center')
-
 
 with open('data/explog.csv', 'w') as logfile:
 
@@ -55,24 +54,25 @@ with open('data/explog.csv', 'w') as logfile:
     for tt in range(1, config.NUM_TRIALS + 1):
 
         xrot = random.choice(config.ROT_STARTS)
+        xrot_offset = random.choice(config.ROT_OFFSETS)
+
         for stim in stims:
             stim.rotation.x = xrot
-        xrot_offset = random.choice(config.ROT_OFFSETS)
         for stim in [stim_orig2, stim_flipped]:
             stim.rotation.x += xrot_offset
 
-        condition = random.choice([condA, condB])
-        scene.meshes = condition
-
         # Draw Fixation Cross
+        scene.bgColor = config.BGCOLOR
         scene.clear()
         fixcross.draw()
         window.flip()
         time.sleep(1.)
 
-
+        # Draw First set of stimuli
+        scene.meshes = random.choice([condA, condB])
         with rc.default_shader:
             scene.draw()
+        fixcross.draw()
         window.flip()
 
         # Collect Response
@@ -85,40 +85,30 @@ with open('data/explog.csv', 'w') as logfile:
             sys.exit()
         assert 'left' in resp or 'right' in resp
 
-        correct_resp = 'left' if condition == condA else 'right'
+        correct_resp = 'left' if scene.meshes == condA else 'right'
         correct = correct_resp in resp
 
         # Log Trial Data
         trialdata = {'Trial': tt,
-                     'Match': True if condition == condA else False,
+                     'Match': True if scene.meshes == condA else False,
                      'RotationA': xrot,
                      'RotationB': xrot + xrot_offset,
                      'Correct': correct,
                      'RT': response_time}
         logwriter.writerow(trialdata)
 
-        # Provide Feedback for Response
+        # Provide Feedback for Response by rotating stimuli to same orientation
         for rot in np.linspace(xrot, xrot + xrot_offset, 15):
             stim_orig.rotation.x = rot
-            stim_orig.update()
             with rc.default_shader:
                 scene.draw()
             window.flip()
+            time.sleep(.016)
 
-        feedback_color = (0., .5, 0.) if correct else (.5, 0., 0.)
-        for color, pauseTime in zip([feedback_color, config.BGCOLOR], [1.5, .001]):
-            scene.bgColor = color
-            with rc.default_shader:
-                scene.draw()
-
-            window.flip()
-            time.sleep(pauseTime)
-
-
-
-
-
-
-
-
+        # Display green if correct and red if incorrect.
+        scene.bgColor =  (0., .5, 0.) if correct else (.5, 0., 0.)
+        with rc.default_shader:
+            scene.draw()
+        window.flip()
+        time.sleep(1.5)
 
