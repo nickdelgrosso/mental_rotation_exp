2c2f62d516d736554e42940bed8bf7627b7bc52c
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-05-19 21:57:58 +0200

    syntax error fix

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ stim_orig = reader.get_mesh('OrigStim', position=(-.25, 0., -1), rotation=(0, co
 stim_orig2 = reader.get_mesh('OrigStim', position = (.25, 0., -1), rotation=(0, config.YROT, 0), scale=config.SCALE)
 stim_flipped = reader.get_mesh('FlippedStim', position = (.25, 0., -1), rotation=(0, config.YROT, 0), scale=config.SCALE)
 
-scene = rc.Scene(meshes=[], camera=rc.Camera(projection=rc.OrthoProjection(origin='center', coords='relative'))
+scene = rc.Scene(meshes=[], camera=rc.Camera(projection=rc.OrthoProjection(origin='center', coords='relative')))
 scene.light.position.z = 20
 
 # Make Groups of Stimuli to refer to in experiment
